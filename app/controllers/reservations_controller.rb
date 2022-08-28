8b34dcee45dd043df7a8444e295c5e6746600ef1
Christopher Ho <christopherho1994@gmail.com>
2022-08-28 18:58:48 +0800

    Done up the Reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,32 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(studio_params, room_params)
+    if @reservation.save
+      redirect_to reservation_path(@reservation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_id)
+      redirect_to(@reservation)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    Reservation.destroy
+    redirect_to reservation_path(@reservation), status: :see_other
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # get "properties", to: "properties#index"
-  # get "properties", to: "properties#show"
-  # post "properties", to: "properties#create"
-  # delete "properties", to: "properties#destroy"
-  # patch "properties", to: "properties#update"
 
   resources :studios do
     resources :reservations, only: %i[show create new update edit]
@@ -20,23 +15,6 @@ Rails.application.routes.draw do
 
   # *TODO pundit
   resources :reviews, only: %i[destroy]
-
-  # get "account", to: "pages#account"
-  # get "account", to: "account#edit"
-  # post "account", to: "account#create"
-
-  # get "reservations", to: "reservations#index"
-  # get "reservations", to: "reservations#show"
-  # get "reservations", to: "reservations#edit"
-  # get "reservations", to: "reservations#new"
-  # post "reservations", to: "reservations#create"
-  # delete "reservations", to: "reservations#show"
-
-  # get "reviews", to: "reviews#new"
-  # post "reviews", to: "reviews#create"
-  # get "reviews", to: "reviews#show"
-
-  # get "host", to: "host#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
