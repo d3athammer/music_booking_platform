42dfb443f6866782ed7af04b526c8d877e589b02
Christopher Ho <christopherho1994@gmail.com>
2022-09-07 20:13:32 +0800

    Done with Registration FrontEnd

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_083753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
