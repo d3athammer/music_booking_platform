51cd1dd62ca2dac51468dd3fa93f76c0c8226664
Christopher Ho <christopherho1994@gmail.com>
2022-09-10 16:34:14 +0800

    Completed Sign In & Login Pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,7 +17,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to studio_path(@review.studio), status :see_other
+    redirect_to studio_path(@review.studio), status: :see_other
   end
 
   private
