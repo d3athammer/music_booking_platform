fad042366ca60000ed61b2385f896e1b987059df
ting wei yap <yap.tingwei@gmail.com>
2022-09-12 23:16:52 +0800

    calculating date, and creating reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,6 +10,7 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
     @timeslot = Timeslot.all
+    @hour_array = hourly_array
     @timeslot_array = []
     @timeslot.each do |time|
       @timeslot_array << [time.time, time.id]
@@ -17,27 +18,24 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @timeslot = Timeslot.all
     @reservation = Reservation.new(reservation_params)
     @reservation.timeslot_id = @reservation.start_time
     @start_time = Timeslot.find(@reservation.timeslot_id).time
     # covert start time and date into datetime
-    # add duration to start time in datetime (to prevent overnight booking bug)
-    # convert end time and date back
-    @time = DateTime.parse "#{@reservation.date}T#{@start_time}.00+08:00"
-    # @time = Time.new(params[:reservation]["date(1i)"],
-    #                  params[:reservation]["date(2i)"],
-    #                  params[:reservation]["date(3i)"],
-    #                  @start_time)
-    @end_time =
+    @datetime = DateTime.parse "#{@reservation.start_date}T#{@start_time}+08:00"
+    # adding time to calculate @reservation.end_date
+    @end_date = @datetime + (@reservation.duration / 24r)
+    # assigning all of these into dates and times
+    # add the rest of the timeslots based on my duration
+    @timeslot_id_array = []
+    # query start_time < = or > = of end_time in timeslot table to get id of timeslot
+    # create a reservation for each timeslot
+    @reservation.end_time = @end_date.strftime("%H:%M")
     @reservation.room = @room
     @reservation.user = current_user
-    @reservation.end_time = @end_time
-    raise
     if @reservation.save
-      TimeslotReservations.create(date: @reservation.date,
-                                  timeslot_id:,
-                                  reservation_id: params[:id],
-                                  user_id: @reservation.user)
+      new_timeslot_reservations(@timeslot_id_array, @reservations)
       redirect_to room_reservations_path(@room)
     else
       render :new, status: :unprocessable_entity
@@ -61,37 +59,34 @@ class ReservationsController < ApplicationController
 
   private
 
-  def end_time
-    # "#{@reservation.start_time}"
-    start_time = "00:30"
-    @end_time = "#{@reservation.date} #{start_time}".to_datetime
+  def new_timeslot_reservations(timeslot_array, reservation)
+    # loop through timeslot id(in an array), loop through this array and save each timeslot into timeslots reservations
+    timeslot_array.each do |timeslot_id|
+      TimeslotReservations.create(
+        start_date: reservation.start_date,
+        end_date: reservation.end_date,
+        timeslot_id:,
+        reservation_id: reservation.id,
+        user_id: reservation.user
+      )
+    end
   end
 
   def set_room
     @room = Room.find(params[:room_id])
   end
 
-  def create_datetime
-    @time = Time.new(params[:reservation]["date(1i)"],
-                     params[:reservation]["date(2i)"],
-                     params[:reservation]["date(3i)"],
-                     @start_time)
-  end
-  # def set_timeslot
-  #   @reservation = Reservation.new(reservation_params)
-  #   @reservation.timeslot_id = @reservation.start_time
-  #   @reservation.timeslot = @timeslot
-  #   @timeslot = Timeslot.find(params[:timeslot_id])
-  # end
-
   def reservation_params
-    params.require(:reservation).permit(:start_time, :duration, :date, :timeslot_id)
+    params.require(:reservation).permit(:start_time, :duration, :start_date, :timeslot_id)
   end
 
-  # def room_params
-  #   params.require(:room).permit(:price, :date, :room_size,
-  #                                :room_type, :description, :total_occupancy,
-  #                                :studio_id, :user_id)
-  # end
-
+  def hourly_array
+    @hour_array = []
+    time = 1
+    24.times do
+      @hour_array << ["#{time} hour", time]
+      time += 0.5
+    end
+    return @hour_array
+  end
 end
