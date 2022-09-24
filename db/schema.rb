9ffd7824b7cbcf6e606c7d28f781f8f35d876e6e
Daniel Goh <daniel.goh@kaligo.com>
2022-09-24 11:33:46 +0800

    Add missing users migration

--- db/schema.rb
+++ db/schema.rb
@@ -153,8 +153,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_163944) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
