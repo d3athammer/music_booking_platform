d63a7a95acdddc9b770534fec47fa6da86bace4a
Christopher Ho <christopherho1994@gmail.com>
2022-10-09 23:40:43 +0800

    Added Authentications

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "@popperjs/core"
 import "bootstrap"
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
