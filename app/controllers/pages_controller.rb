26e4973d93de443011b05ec3d343d6890b0eccfb
ting wei yap <yap.tingwei@gmail.com>
2022-10-18 00:57:17 +0800

    privatized hour_array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,32 +5,38 @@ class PagesController < ApplicationController
     @studio = Studio.all
     @room = Room.all
     @timeslot_reservations = TimeslotReservation.all
-    @timeslots = Timeslot.all
-    @timeslot_array = []
-    @timeslots.each do |time|
-      @timeslot_array << [time.time, time.id]
-    end
-
+    @timeslot_array = timeslot_array
     @hour_array = hourly_array
-    # @timeslot_array = []
-    # @timeslot.timeslot.each do |time|
-    # @timeslot_array << [time.time, time.id]
+  end
+
+  def show
+
   end
 
   private
 
   def hourly_array
-    @hour_array = []
+    hour_array = []
     time = 1
     23.times do
       if time <= 1
-        @hour_array << ["#{time} hour", time]
+        hour_array << ["#{time} hour", time]
         time += 0.5
       else
-        @hour_array << ["#{time} hours", time]
+        hour_array << ["#{time} hours", time]
         time += 0.5
       end
     end
-    return @hour_array
+    return hour_array
   end
+
+  def timeslot_array
+    timeslot_array = []
+    timeslot = Timeslot.all
+    timeslot.each do |time|
+      timeslot_array << [time.time, time.id]
+    end
+    return timeslot_array
+  end
+
 end
